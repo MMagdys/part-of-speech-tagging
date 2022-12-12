eab0cd2f3d93cc860996c01b9a795b4f9f21cc4b
mmagdys <muhammad.magdy50@gmail.com>
2022-12-12 02:03:44 +0200

    [+] Add scores seed

--- partOfSpeech_backend/src/seeds/WordSeed.ts
+++ partOfSpeech_backend/src/seeds/WordSeed.ts
@@ -8,12 +8,14 @@ import { IWordRepository } from '@pbb/repositories/WordRepository';
 import fs from 'fs';
 import mongoose from 'mongoose';
 import { IWordProps } from '@pbb/models/word/IWord';
+import { IRankProps } from '@pbb/models/rank/IRank';
+import { IRankRepository } from '@pbb/repositories/RankRepository';
 
 
 
 const saveWordSeedFromFile = async () => {
 
-    const collectionName = 'Words';
+    const collectionName = 'words';
     await mongoose.connect(config.mongoUrl);
     const collections = await mongoose.connection.db.listCollections({ name: collectionName }).toArray();
     const isExist = collections.length == 1;
@@ -32,8 +34,8 @@ const saveWordSeedFromFile = async () => {
         
         for (let i = 0; i < words.length; i++) {
 
-            const word = words[i] as IWordProps;
-            let wordProps: IWordProps = {
+            const word = words[i];
+            const wordProps: IWordProps = {
                 word: word.word,
                 pos: word.pos
             }
@@ -42,8 +44,47 @@ const saveWordSeedFromFile = async () => {
     });
 }
 
+const saveRankSeedFromFile = async () => {
 
-saveWordSeedFromFile().then(() => {
+    const collectionName = 'ranks';
+    await mongoose.connect(config.mongoUrl);
+    const collections = await mongoose.connection.db.listCollections({ name: collectionName }).toArray();
+    const isExist = collections.length == 1;
+    if(isExist) {
+        await mongoose.connection.db.dropCollection(collectionName);
+    }
+
+    const rankRepository = container.get<IRankRepository>(TYPES.IRankRepository);
+    let ranks: any[];
+
+    fs.readFile('./TestData.json', 'utf8', async function (err: any, data: any) {
+
+        if (err) throw err;
+
+        ranks = JSON.parse(data).scoresList;
+
+        for (let i = 0; i < ranks.length; i++) {
+
+            const rank = ranks[i];
+
+            const rankProps: IRankProps = {
+                score: (rank as number)
+            }
+            await rankRepository.save(rankProps)
+        }
+    });
+}
+
+
+const run = async () => {
+    await Promise.all([
+        saveWordSeedFromFile(),
+        saveRankSeedFromFile()
+    ])
+}
+
+
+run().then(() => {
     console.log("Seed successfull");
 
 }).catch((error) => {
